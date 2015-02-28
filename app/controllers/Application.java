0597a84e033be774d691d0da8be50b4e8f35c31c
NAGI <Nagisama.j@gmail.com>
2015-03-01 01:55:42 +0700

    modified login system

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,36 +1,46 @@
 package controllers;
 
-import play.data.*;
-import play.mvc.*;
-import views.html.*;
 import models.User;
+import play.api.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 import static play.data.Form.form;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(views.html.index.render("hello"));
     }
 
     public static Result login() {
-        return ok(
-                login.render(form(Login.class))
-        );
+        return ok(views.html.login.render(Form<Login> form(Login.class)));
     }
-}
 
-public static class Login {
+    public static class Login {
 
-    public String username;
-    public String password;
+        public String username;
+        public String password;
 
-    public String validate() {
-        if(User.authenticate() == null) {
-            return "Invalid user or password";
+        public String validate() {
+            if(User.authenticate(username,password) == null) {
+                return "Invalid user or password";
+            }
+
+            return null;
         }
 
-        return null;
+        public static Result authenticate() {
+            Form<Login> loginForm = form(Login.class).bindFromRequest();
+            if (loginForm.hasErrors()) {
+                return badRequest(views.html.login.render(loginForm));
+            } else {
+                session().clear();
+                session("username", loginForm.get().username);
+                return redirect(
+                        routes.Application.index()
+                );
+            }
+        }
     }
-
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,22 +1,9 @@
 package models;
 
-/**
- * Created by NAGISAMA on 2/28/15 AD.
- */
-
-import play.data.Form;
 import play.db.ebean.Model;
-import play.mvc.Result;
-
-import controllers.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
-import static play.data.Form.form;
-import static play.mvc.Controller.session;
-import static play.mvc.Results.badRequest;
-import static play.mvc.Results.redirect;
 
 @Entity
 public class User extends Model {
@@ -36,17 +23,8 @@ public class User extends Model {
             String.class, User.class
     );
 
-    public static Result authenticate() {
-        Form<Login> loginForm = form(Login.class).bindFromRequest();
-        if (loginForm.hasErrors()) {
-            return badRequest(Login.render(loginForm));
-        } else {
-            session().clear();
-            session("username", loginForm.get().username);
-            return redirect(
-                    routes.Application.index()
-            );
-        }
-    }
 
+    public static Object authenticate(String username, String password) {
+        return null;
+    }
 }
