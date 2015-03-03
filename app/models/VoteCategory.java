2a59155f036644405194ac27af6b453249fcd37a
Guro <greenblood11@hotmail.com>
2015-03-03 11:34:56 +0700

    modify database

--- app/models/VoteCategory.java
+++ app/models/VoteCategory.java
@@ -8,14 +8,13 @@ import play.db.ebean.Model;
 @Entity
 @Table(name = "vote_category")
 public class VoteCategory extends Model{
-	@Id
-	public Long id;
-	public int score = 1;
-	
-	@Constraints.Required
-	public String name;
-	 
-	public static Finder<Long, VoteCategory> find = new Finder<Long, VoteCategory>(
-			Long.class, VoteCategory.class);
+    @Id
+    public Long id;
+
+    @Constraints.Required
+    public String name;
+
+    public static Finder<Long, VoteCategory> find = new Finder<Long, VoteCategory>(
+            Long.class, VoteCategory.class);
 
 }
