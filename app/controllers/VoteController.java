c2031839e8cfc3b3233b3da01f5e0067787fbefd
Guro <greenblood11@hotmail.com>
2015-03-07 01:05:22 +0700

    add view vote result page

--- app/controllers/VoteController.java
+++ app/controllers/VoteController.java
@@ -29,11 +29,15 @@ public class VoteController extends Controller {
 	}
 	
     public static Result vote() {
-        User user = User.find.byId(1);
+        User user = User.find.byId(2);
         Project project = Project.find.byId(1);
         List<VoteCategory> voteCategories = VoteCategory.find.all();
 
         return ok(vote.render(user,project,voteCategories));
     }
-	
+
+    public static Result result() {
+        List<Vote> voteList = Vote.find.all();
+        return ok(views.html.result.render(voteList));
+    }
 }
