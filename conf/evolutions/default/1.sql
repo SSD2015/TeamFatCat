0b339a1fd827fdae3a244df5bf3876aebfb550e1
Mxyz <iam_max21@hotmail.com>
2015-04-20 10:47:31 +0700

    pull

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -83,23 +83,23 @@ create index ix_rate_project_3 on rate (project_id);
 
 SET FOREIGN_KEY_CHECKS=0;
 
-drop table deadline;
-
-drop table image;
-
-drop table project;
-
-drop table rate;
-
-drop table vote_category;
-
-drop table team;
-
-drop table user;
-
-drop table vote;
-
-drop table vote_category;
+# drop table deadline;
+#
+# drop table image;
+#
+#  drop table project;
+#
+#  drop table rate;
+#
+#  drop table vote_category;
+#
+#  drop table team;
+#
+#  drop table user;
+#
+#  drop table vote;
+#
+#  drop table vote_category;
 
 SET FOREIGN_KEY_CHECKS=1;
 
